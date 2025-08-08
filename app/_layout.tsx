6083f59dc052f2d0d311c2f5018a670d3a6f46f7
GiladKhatin <GiladKhaytin@gmail.com>
2025-08-08 15:14:41 +0300

    background and bottom bar

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -12,7 +12,7 @@ export default function TabLayout() {
         },
       }}
     >
-      <Tabs.Screen name="index" options={{title: 'Home', tabBarIcon: ({ color }) => (<FontAwesome size={28} name="home" color={color} />),
+      <Tabs.Screen name="index" options={{title: 'Home', tabBarIcon: ({ color }) => (<FontAwesome size={28} name="home" color={'red'} />),
       }}
       />
     </Tabs>
