e7b46d57c00e92eb13ebfbe495a4789b441f6a37
GiladKhatin <GiladKhaytin@gmail.com>
2025-08-08 15:12:05 +0300

    background and bottom bar

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,20 @@
-import { Stack } from "expo-router";
+import FontAwesome from '@expo/vector-icons/FontAwesome';
+import { Tabs } from 'expo-router';
 
-export default function RootLayout() {
-  return <Stack />;
+export default function TabLayout() {
+  return (
+    <Tabs
+      screenOptions={{
+        headerShown: false,
+        tabBarActiveTintColor: 'blue',
+        tabBarStyle: {
+          backgroundColor: '#fff',
+        },
+      }}
+    >
+      <Tabs.Screen name="index" options={{title: 'Home', tabBarIcon: ({ color }) => (<FontAwesome size={28} name="home" color={color} />),
+      }}
+      />
+    </Tabs>
+  );
 }
--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,24 @@
-import { Text, View } from "react-native";
+import background from "@/assets/images/background.png";
+import { ImageBackground, StyleSheet, View } from 'react-native';
+
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+    <View style={styles.container}>
+      <ImageBackground source={background} resizeMode="cover" style={styles.background}></ImageBackground>
     </View>
   );
-}
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  background: {
+    width: '100%',
+    height: '100%',
+    flex: 1,
+    resizeMode: 'cover',
+    justifyContent: 'center'
+  },
+});
