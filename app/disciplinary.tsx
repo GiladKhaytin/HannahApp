53d07f456d8706cd862f82d6303236c02510b136
GiladKhatin <GiladKhaytin@gmail.com>
2025-09-05 13:27:00 +0300

    Click and Scroll to top

--- app/disciplinary.tsx
+++ app/disciplinary.tsx
@@ -1,12 +1,21 @@
 import Hannah from '@/components/Hannah';
 import disciplinaryRegulations from '@/data/dir.json';
+import { useScrollToTop } from '@react-navigation/native';
 import React from 'react';
 import { View } from 'react-native';
 
 export default function Disciplinary() {
+  const ref = React.useRef(null);
+
+  useScrollToTop(
+    React.useRef({
+      scrollToTop: () => ref.current?.scrollTo({ y: 0, animated: true}),
+    })
+  );
+    
   return (
     <View style={{ flex: 1 }}>
-      <Hannah data={disciplinaryRegulations} />
+      <Hannah ref={ref} data={disciplinaryRegulations} />
     </View>
   );
 }
--- app/traffic.tsx
+++ app/traffic.tsx
@@ -1,7 +1,7 @@
-import Hannah from '@/components/Hannah';
-import disciplinaryRegulations from '@/data/traffic.json';
-import React from 'react';
-import { View } from 'react-native';
+import Hannah from "@/components/Hannah";
+import disciplinaryRegulations from "@/data/traffic.json";
+import React from "react";
+import { View } from "react-native";
 
 export default function Disciplinary() {
   return (
