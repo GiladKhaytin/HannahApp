1c15c8300f3e4536589714bff443e68362d076a2
GiladKhatin <GiladKhaytin@gmail.com>
2025-09-05 14:59:45 +0300

    update code

--- app/disciplinary.tsx
+++ app/disciplinary.tsx
@@ -1,21 +1,9 @@
 import Hannah from '@/components/Hannah';
 import disciplinaryRegulations from '@/data/dir.json';
-import { useScrollToTop } from '@react-navigation/native';
 import React from 'react';
-import { View } from 'react-native';
 
-export default function Disciplinary() {
-  const ref = React.useRef(null);
-
-  useScrollToTop(
-    React.useRef({
-      scrollToTop: () => ref.current?.scrollTo({ y: 0, animated: true}),
-    })
-  );
-    
+export default function Disciplinary() { 
   return (
-    <View style={{ flex: 1 }}>
-      <Hannah ref={ref} data={disciplinaryRegulations} />
-    </View>
+    <Hannah  data={disciplinaryRegulations} />
   );
 }
