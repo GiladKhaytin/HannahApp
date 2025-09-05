382d50995e57752dd3cd14aab656baff3770b2d5
GiladKhatin <GiladKhaytin@gmail.com>
2025-09-05 15:07:26 +0300

    update code

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -4,7 +4,7 @@ import { Tabs } from 'expo-router';
 
 export default function TabLayout() {
   return (
-    <Tabs screenOptions={{headerShown: false, tabBarActiveTintColor: 'cyan', tabBarStyle: { backgroundColor: '#e69933'}}}>
+    <Tabs screenOptions={{headerShown: false, tabBarActiveTintColor: 'white', tabBarStyle: { backgroundColor: '#DB7520'}}}>
         <Tabs.Screen name="index" options={{title: 'דף הבית', tabBarIcon: ({ color }) => (<FontAwesome size={28} name="home" color={color} />),}}/>
         <Tabs.Screen name="disciplinary" options={{title: 'אכיפת דיגום', tabBarIcon: ({ color }) => (<FontAwesome5 name="tshirt" size={24} color={color}/>),}}/>
         <Tabs.Screen name="traffic" options={{title: 'אכיפת תנועה', tabBarIcon: ({ color }) => (<FontAwesome5 name="car" size={24} color={color}/>),}}/>
